b60d8048bf2a143ce636db5cabc2ba0b9be22ba4
Audry Wolters <audrywolters@gmail.com>
2022-03-15 09:40:53 -0500

    added icon from Expo vector-icons library

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 const SearchBar = () => {
 	return (
 		<View style={[styles.background, styles.shape]}>
+			<Feather name="search" size={30} />
 			<Text>SearchBar</Text>
 		</View>
 	)
