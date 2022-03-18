e09d2fabdf9083e1d5236256d02c4741271381ab
Audry Wolters <audrywolters@gmail.com>
2022-03-18 15:57:25 -0500

    exported search logic to useResults hook file

---
+++ src/hooks/useResults.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import yelp from '../api/yelp'
+
+export default () => {
+	const [results, setResults] = useState([])
+	const [errorMessage, setErrorMessage] = useState('')
+	
+	// call to the api - search for restaurants with a short phrase
+	const searchApi = async (searchTerm) => {
+		try {
+			const response = await yelp.get('/search', {
+				params: {
+					limit: 50,
+					term: searchTerm,
+					location: 'san jose'
+				}
+			})
+			setResults(response.data.businesses)
+		} catch (e) {
+			setErrorMessage('Something went wrong')
+		}
+	}
+
+	// call searchApi once on load (for some data to display when loaded)
+	// empty array says 'only run this once, when rendered'
+	useEffect(() => {
+		searchApi('pasta')
+	}, [])
+	
+	return [searchApi, results, errorMessage]
+
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,41 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState('')
-	const [results, setResults] = useState([])
-	const [errorMessage, setErrorMessage] = useState('')
-
-	// call searchApi when component is first rendered
-	// empty array says 'only run this once, when compnent is rendered'
-	useEffect(() => {
-		searchApi('bip')
-	}, [])
-
-
-	const searchApi = async (searchTerm) => {
-		try {
-			const response = await yelp.get('/search', {
-				params: {
-					limit: 50,
-					term: searchTerm,
-					location: 'san jose'
-				}
-			})
-			setResults(response.data.businesses)
-		} catch (e) {
-			setErrorMessage('Something went wrong')
-		}
-	}
+	const [searchApi, results, errorMessage] = useResults()
 
 	return (
 		<View style={styles.parentView}>
-			<SearchBar 
+			<SearchBar
 				term={term}
-				onTermChange={setTerm} 
-				onTermSubmit={() => searchApi(term)} 
+				onTermChange={setTerm}
+				onTermSubmit={() => searchApi(term)}
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>We have found {results.length} results.</Text>
@@ -46,7 +23,6 @@ const SearchScreen = () => {
 const styles = StyleSheet.create({
 	parentView: {
 		backgroundColor: 'white',
-		// alignItems: 'flex-start',
 		flex: 1
 	}
 })
