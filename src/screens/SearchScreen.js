f47eedd995b165a6a801439ee6395f0a0c0f0241
Audry Wolters <audrywolters@gmail.com>
2022-03-19 10:33:28 -0500

    changing to using empty brackets for most parent element. now I don't know how to change BG color. will another day.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import ResultsList from '../components/ResultsList'
 import useResults from '../hooks/useResults'
+import react from 'react'
+import { NavigationEvents } from 'react-navigation'
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState('')
@@ -18,7 +20,9 @@ const SearchScreen = () => {
 
 	// UI JSX
 	return (
-		<View style={styles.parentView}>
+		// empty brackets are better practive for react-native
+		// will find out how to make most parent BG color another day
+		<>
 			{/* search bar and some search results data */}
 			<SearchBar
 				term={term}
@@ -34,15 +38,19 @@ const SearchScreen = () => {
 				<ResultsList results={filterResultsByPrice('$$')}  title="Bit Pricier" />
 				<ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
 			</ScrollView>
-		</View>
+		</>
 	)
 }
 
 const styles = StyleSheet.create({
-	parentView: {
-		backgroundColor: 'white',
-		flex: 1
-	}
+	// not using this for now
+	// i will when i find out how to target most parent element
+	// or at least how to change BG color
+	
+	// parentView: {
+	// 	backgroundColor: 'white',
+	// 	// for web - flex: 1
+	// }
 })
 
 export default SearchScreen
