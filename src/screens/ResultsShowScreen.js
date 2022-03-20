4becc2aee6b52d7ebaa643667c1f338d285efa13
Audry Wolters <audrywolters@gmail.com>
2022-03-20 10:50:05 -0500

    now showing FlatList of ResultDetail's images

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({ navigation }) => {
 	// null is used for an object as oppossed to {}
 	const [result, setResult] = useState(null)
-	console.log(result)
-	
+
+	// getParam -> id is coming from ResultsList
 	const id = navigation.getParam('id')
 
 	const getResult = async (id) => {
@@ -14,17 +14,36 @@ const ResultsShowScreen = ({ navigation }) => {
 		setResult(response.data)
 	}
 
+	// call this once on load
 	useEffect(() => {
 		getResult(id)
 	}, [])
 
+	// if getting the result wasn't succesful, keep it empty
+	if (!result) {
+		// make an error message
+		return null
+	}
+
 	return (
 		<>
-			<Text>ResultsShowScreen</Text>
+			<Text>{result.name}</Text>
+			<FlatList
+				data={result.photos}
+				keyExtractor={(photo) => photo}
+				renderItem={({ item }) => {
+					return  <Image source={{ uri: item }} style={styles.image} />
+				}}
+			/>
 		</>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	image: {
+		height: 200,
+		width: 300
+	}
+})
 
 export default ResultsShowScreen
