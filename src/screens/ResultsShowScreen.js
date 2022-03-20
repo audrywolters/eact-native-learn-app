6846761de0b5e66e5a620590a633e877317654d8
Audry Wolters <audrywolters@gmail.com>
2022-03-20 10:22:21 -0500

    withNavigation we have access to the details id. now we are hitting api to get get that business's data.

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import yelp from '../api/yelp'
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+	// null is used for an object as oppossed to {}
+	const [result, setResult] = useState(null)
+	console.log(result)
+	
+	const id = navigation.getParam('id')
+
+	const getResult = async (id) => {
+		const response = await yelp.get(`/${id}`)
+		setResult(response.data)
+	}
+
+	useEffect(() => {
+		getResult(id)
+	}, [])
+
 	return (
 		<>
 			<Text>ResultsShowScreen</Text>
